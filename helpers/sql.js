a51049714bc6f29a12c70fddd42d0cef1dc8570d
Lizzy Ahler <ahlerliz@gmail.com>
2021-10-06 12:29:13 -0700

    added docstring to helper/sql.js and added helper/sql.test.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,10 +1,21 @@
 "use strict";
 const { BadRequestError } = require("../expressError");
 
-// TODO:THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Helper Function: Convert JS input data to sql data for update requests.
+ *
+ * Allows users to input only the JSON data for the keys and
+ * values that they want to update. If input column names are camelCase, this converts them to snake_case. 
+ * Returns object of columns to change and their new values (see example below)
+ *
+ * Ex input: { "name": "Hall-Davis1", "description": "A company that designs halls", "numEmployees": 65} ====>
+ *    output: {setCols: ' "name"=$1, "description"=$2, "num_employees"=$3 ',  
+ *            values:[ 'Hall-Davis1', 'A company that designs halls', 65 ]}
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  
   const keys = Object.keys(dataToUpdate);
+
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
---
+++ helpers/sql.test.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const jwt = require("jsonwebtoken");
+const { sqlForPartialUpdate } = require("./sql");
+const { SECRET_KEY } = require("../config");
+
+
+describe("convertInputToSql", function () {
+    test("works: company update", function () {
+        const input = { 
+            "name": "Hall-Davis1", 
+            "description": "A company that designs halls", 
+            "numEmployees": 65}
+        const jsToSql = {
+            numEmployees: "num_employees",
+            logoUrl: "logo_url",
+          }
+        const output = sqlForPartialUpdate(input, jsToSql);
+        expect(output).toEqual({
+            setCols: '"name"=$1, "description"=$2, "num_employees"=$3',  
+            values: [ 'Hall-Davis1', 'A company that designs halls', 65 ]
+        });
+    });
+
+    test("works: user update", function () {
+        const input = { 
+            "firstName": "Lizzy", 
+            "lastName": "Ahler", 
+            "email": "email@email.com"}
+        const jsToSql = {
+            firstName: "first_name",
+            lastName: "last_name",
+            isAdmin: "is_admin",
+          }
+        const output = sqlForPartialUpdate(input, jsToSql);
+        expect(output).toEqual({
+            setCols: '"first_name"=$1, "last_name"=$2, "email"=$3',  
+            values: [ 'Lizzy', 'Ahler', "email@email.com" ]
+        });
+    });
+});
